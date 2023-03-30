757b7a931622037ec2ddae95c9efe218184e00f3
Boris <boris12lit@gmail.com>
2023-03-30 18:20:29 +0300

    added shallow vs deep copy practice in mechStuff.js

--- src/stuff/mechStuff.js
+++ src/stuff/mechStuff.js
@@ -28,4 +28,48 @@ const isExtraChance = () => {
 
 
 
+// --------WRONG-SHALLOW-COPY----------------
+const stateShallow = [
+    { currentLvl: 10, currentExp: 0, label: 'Сила'},
+    { currentLvl: 10, currentExp: 0, label: 'Выносливость'},
+    { currentLvl: 10, currentExp: 0, label: 'Ловкость'},
+]
 
+const newStateShallow = [
+    ...stateShallow.slice(0, 1),
+    {...stateShallow[1],
+        currentLvl: 111,
+        currentExp: 111
+    },
+    ...stateShallow.slice(2)
+]
+
+stateShallow[1].currentExp = 2222
+stateShallow[2].currentExp = 2222
+
+console.log(stateShallow)
+console.log(newStateShallow)
+
+// --------RIGHT-DEEP-COPY----------------
+const stateDeep = [
+    { currentLvl: 10, currentExp: 0, label: 'Сила'},
+    { currentLvl: 10, currentExp: 0, label: 'Выносливость'},
+    { currentLvl: 10, currentExp: 0, label: 'Ловкость'},
+]
+
+const newStateBuff = stateDeep.map(el => ({...el}))
+
+const newStateDeep = [
+    ...newStateBuff.slice(0, 1),
+    {...newStateBuff[1],
+        currentLvl: 111,
+        currentExp: 111
+    },
+    ...newStateBuff.slice(2)
+]
+
+stateDeep[1].currentExp = 2222
+stateDeep[2].currentExp = 2222
+
+console.log(stateDeep)
+console.log(newStateDeep)
